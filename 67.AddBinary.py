4711b3df3fa55fc69dc279937b8066d79a64c55c
Karen <ka.spk27@gmail.com>
2023-01-31 16:00:06 +0300

    Update 67.AddBinary.py

--- 67.AddBinary.py
+++ 67.AddBinary.py
@@ -10,16 +10,31 @@ Output: "10101"
 """
 
 
-def addBinary(self, a, b):
+def addBinary(a, b):
     """
     :type a: str
     :type b: str
     :rtype: str
     """
-    for
+
+    res = ''
+    x = int(a) + int(b)
+    if x == 0:
+        return '0'
+    tmp = 0
+    for i in reversed(str(x)):
+        if int(i) + tmp != 2:
+            res = '1' + res
+            tmp = 0
+        else:
+            res = '0' + res
+            tmp = 1
+
+    if tmp == 1:
+        res = '1' + res
+    return res
 
 
-
-a = '11'
-b = '1'
+a = '1010'
+b = '1011'
 print(addBinary(a,b))
