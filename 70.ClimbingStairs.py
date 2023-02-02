1de5ae4e8d61259417e99efd5dd7c3f6e6fa7908
Karen <ka.spk27@gmail.com>
2023-02-02 12:04:18 +0300

    Update 70.ClimbingStairs.py

--- 70.ClimbingStairs.py
+++ 70.ClimbingStairs.py
@@ -18,30 +18,36 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 
+:type n: int
+:rtype: int
 """
 
-def climbStairs(n):
-    """
-    :type n: int
-    :rtype: int
-    """
+def climbStairs_0(n):
+     return  1 if n <= 1 else climbStairs_0(n - 1) + climbStairs_0(n - 2) 
 
-  
-    #return  1 if n <= 1 else climbStairs(n - 1) + climbStairs(n - 2) 
-
+def climbStairs_1(n):
     if n <= 3:
         return n
     x = 3
     y = 2
-    
     while(n > 3):
         x = x + y
         y = x - y
         n -= 1
+    return x 
+
+def climbStairs_2(n):
+    if n <= 3:
+        return n
+    x = 3
+    y = 2
+    for i in range(3, n):
+        x = x + y
+        y = x - y
     return x
 
 x = 45
-print(climbStairs(x))
-assert(climbStairs(4)) == 5, 4
-assert(climbStairs(5)) == 8, 5
-assert(climbStairs(7)) == 21, 7
+print(climbStairs_0(x))
+assert(climbStairs_0(4)) == 5, 4
+assert(climbStairs_1(5)) == 8, 5
+assert(climbStairs_2(7)) == 21, 7
